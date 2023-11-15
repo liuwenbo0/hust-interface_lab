0793bb91dfd198b1f3e3eb4d9a061103b3ed96d2
webliu <3097365481@qq.com>
2023-11-15 23:49:29 +0800

    lab3

---
+++ example_gpio_C/src/gpio_demo.c
@@ -0,0 +1,22 @@
+#define IO_LEDR 0x80100000
+#define IO_SW 0x80100008
+#define READ_GPIO(dir) (*(volatile unsigned *)dir)
+#define WRITE_GPIO(dir, value) { (*(volatile unsigned *)dir) = (value); }
+int main ( void )
+{
+int i, j=1, count=0xF, delay=10000000;
+while (1) {
+WRITE_GPIO(IO_LEDR, count);
+if(j == 1) {
+count = count << 1;
+if(count == 0xf000)
+j = 0;
+} else {
+count = count >> 1;
+if(count == 0x000f)
+j = 1;
+}
+for (i=0; i<delay>>READ_GPIO(IO_SW); i++);
+}
+return(0);
+}
